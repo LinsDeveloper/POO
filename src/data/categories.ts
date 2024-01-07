0715be4a968d3fc165baf0d7c29cef9d4c5b7e9e
Pedro Lins <pedrolins42@gmail.com>
2024-01-07 12:08:40 -0300

    feat(filterDate) Add filter by List

---
+++ src/data/categories.ts
@@ -0,0 +1,9 @@
+import { Category } from '../types/category';
+
+
+export const categories: Category = {
+    food: { title: 'alimentação', color: "blue", expense: true},
+    rent: { title: 'Aluguel', color: "brown", expense: true},
+    salary: { title: 'Salário', color: "brown", expense: false}
+};
+
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    {date: new Date(2021, 9, 15), category: 'food', title: 'Macdonalds', value: 32.12},
+    {date: new Date(2021, 9, 15), category: 'Transporte', title: 'Uber', value: 26.45},
+    {date: new Date(2021, 9, 16), category: 'Transporte', title: 'Uber', value: 45.80},
+    {date: new Date(2021, 9, 17), category: 'Salário', title: 'Salário Act Digital', value: 2200.00}
+];
